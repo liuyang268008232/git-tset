f9c74e4da316047234f844b575044428f0408f91
liuyang <268008232@qq.com>
2021-08-18 15:23:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2021-08-18 15:16
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git!");
+    }
+}
